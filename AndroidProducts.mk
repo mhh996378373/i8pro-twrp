1c1d3c4d5f81e8eb0305a212bf84ce8ca546bd90
mhh996378373 <mhh996378373@users.noreply.github.com>
2024-02-21 21:15:57 +0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+# Replace aiv8167sm3_bsp with your Device Name's Value.
+# Replace alps with your Brand's / Manufacturer's Value.
+# The part of last line in mine looks like "omni_Primo_RX5.mk"
+
+LOCAL_PATH := device/alps/aiv8167sm3_bsp
+
+PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_aiv8167sm3_bsp.mk
